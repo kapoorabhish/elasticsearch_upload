72c446d4242399f669532f6c54c1507639418f13
Tanu91 <tanu.mittal@iic.ac.in>
2017-03-07 22:43:15 +0530

    resolved conflict

--- js/background.js
+++ js/background.js
@@ -1,8 +1,4 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.create({'url': chrome.extension.getURL('index.html')}, function (tab) {
   });
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 36a226a2983528b61dca93d7152d85460db743d3
